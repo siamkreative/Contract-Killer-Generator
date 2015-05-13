ce60a30a0ba0bf1ebb231e89bc48e180729617fe
Julien Vernet <julien@vernet.me>
2015-05-13 18:16:53 +0700

    Add analytics tracking code

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:400,700,300">
 	<link rel="stylesheet" type="text/css" href="app-screen-only.css" class="nopdf">
 	<link rel="stylesheet" type="text/css" href="app.css">
+	<script class="nopdf">
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-15485830-37', 'auto');
+	ga('send', 'pageview');
+	</script>
 </head>
 <body>
 	<!-- http://ironsummitmedia.github.io/startbootstrap-simple-sidebar/ -->
